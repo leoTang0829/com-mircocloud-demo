90613cdadb8d806b7520d5b3384212f0b06bb136
tangmenglong <tangmenglong@iyunxiao.com>
2019-11-06 18:33:56 +0800

    Initial commit

---
+++ src/main/java/com/mircocloud/demo/commircoclouddemo/ComMircocloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mircocloud.demo.commircoclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComMircocloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComMircocloudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mircocloud/demo/commircoclouddemo/ComMircocloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mircocloud.demo.commircoclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComMircocloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
